3acbc5ce1a919bab9aac61c13d7d26d5abc85171
Hajdudev <hajdu2024@gmail.com>
2025-05-14 21:10:45 +0200

    Working on hero page . layout made working

---
+++ app/components/Hero.tsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+  return (
+    <div className="w-screen top-0 -z-1 sticky h-screen">
+
+      <div className="flex w-full h-full">
+        <div className=" w-1/2 h-full bg-amber-200 flex  items-center justify-center">
+          <h1>Hello how are you</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
