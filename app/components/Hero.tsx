e3d2d2e017c9ed337525927db8d8c6d56bcd581e
phls <hajdu2024@gmail.com>
2025-05-19 11:09:25 +0200

    made the hero layout

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,10 +1,21 @@
 export default function Hero() {
   return (
-    <div className="w-screen top-0 -z-1 sticky h-screen">
+    <div className="w-screen top-0 -z-1 sticky h-[80vh]">
+      <div className=" bg-amber-400 h-full flex w-full ">
+        <div className=" w-1/2  flex flex-col items-center justify-center">
+          <div>
+            <h1 className="text-3xl" >From Concepts To Clicks</h1>
+          </div>
+          <div className="mt-10">
+            <p className="text-4xl font-bold">We Build Websites That Drive Results</p>
+          </div>
+          <button className="bg-cobal ">
+            Contact Us
+          </button>
+        </div>
 
-      <div className="flex w-full h-full">
-        <div className=" w-1/2 h-full bg-amber-200 flex  items-center justify-center">
-          <h1>Hello how are you</h1>
+        <div className="w-1/2 " >
+          <h1>There will be a object</h1>
         </div>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,6 @@ import Hero from "./components/Hero";
 export default function Home() {
   return <div>
     <Hero />
-    <div className="w-screen h-screen bg-blue-200"></div>
+    <div className="bg-amber-50 w-full h-screen"></div>
   </div>
 }
