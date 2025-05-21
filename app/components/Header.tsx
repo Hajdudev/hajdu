c98d7b0a0a3a127662f5d5c9ab84d4192a13a789
phls <hajdu2024@gmail.com>
2025-05-21 09:37:20 +0200

    Just checkin

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,40 +3,38 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import Image from "next/image";
 import { useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
-import { setSelected } from "../store/gsap/gsap";
 
 export default function Header() {
   const selected = useSelector((state: RootState) => state.gsap.selected)
-  const dispatch = useDispatch();
-  useGSAP(() => {
-    if (selected == 4){
-    gsap.set(headerRef.current, { y: -100, opacity: 0 });
-    gsap.to(headerRef.current, {
-      y: 0,
-      opacity: 1,
-      duration: 1.2,       
-      ease: "power2.out",  
-      onComplete: () => {
-         dispatch(setSelected(5));
-      }
-    });
-    }
-  },[selected])
   const headerRef = useRef(null)
+
+  useGSAP(() => {
+    if (selected == 3) {
+      gsap.set(headerRef.current, { y: -100, opacity: 0 });
+      gsap.to(headerRef.current, {
+        y: 0,
+        opacity: 1,
+        duration: 1.2,
+        ease: "power2.out",
+      });
+    }
+  }, [selected])
+
   return (
-    <div ref={headerRef} id="header" className="w-screen opacity-0  fixed z-10 flex justify-between items-center px-16 py-4  h-auto">
-      <div className="w-16 h-16  relative">
-        <Image src='/images/Logo.png' priority alt="Logo" fill sizes="cover"/> 
+    <div ref={headerRef} id="header" className="w-screen opacity-0 fixed z-10 flex justify-between items-center px-16 py-4 h-auto">
+      <div className="w-16 h-16 relative">
+        <Image src='/images/Logo.png' priority alt="Logo" fill sizes="cover" />
       </div>
       <div className="flex gap-13 justify-between items-center">
         <span className="text-xl text-paper/80">About</span>
         <span className="text-xl text-paper/80">Services</span>
         <span className="text-xl text-paper/80">Pricing</span>
-        <button className=" bg-gradient-to-r rounded-3xl animate-background-animation from-cobal via-blue-400 to-cobal text-2xl  px-7 py-2">Contact</button>
+        <button className="bg-gradient-to-r from-cobal via-blue-400 to-cobal rounded-3xl text-2xl px-7 py-2 [background-size:200%_200%] ">
+          Contact
+        </button>
       </div>
     </div>
   )
 }
-
