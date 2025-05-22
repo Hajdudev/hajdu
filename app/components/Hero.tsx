4137dd0cbfaceec28724950ea795ab577995ba87
Hajdudev <hajdu2024@gmail.com>
2025-05-22 19:03:32 +0200

    made text for the services

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,7 @@ import HeroText from "../ui/HeroText";
 import HeroTextSub from "../ui/HeroTextSub";
 export default function Hero() {
   return (
-    <div id="hero" className="w-screen  h-[80vh]  -z-1">
+    <div id="hero" className="w-screen  sticky top-0 h-[80vh]  -z-1">
       <div className="fixed right-0 bottom-0 w-screen h-screen -z-10">
         <GradientBall />
       </div>
