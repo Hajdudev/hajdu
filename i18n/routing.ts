f5baaa282132914fb89b8b19c553720adc237e16
Peter Hajdu <hajdu2024@gmail.com>
2025-06-05 10:34:31 +0200

    final changes i hope

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["en", "sk"],
+  locales: ["sk", "en"],
 
   // Used when no locale matches
   defaultLocale: "sk",
